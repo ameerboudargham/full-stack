adb5a74a0a7eb123aeaa79d8120941318bd825b2
ameerboudargham <ameerboudargham@outlook.com>
2024-02-06 22:29:38 +0200

    changes in style by bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,43 @@
 import { useState } from 'react';
-import './App.css';
 import { SampleProducts } from './data';
+import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap';
 
 function App() {
   // eslint-disable-next-line no-empty-pattern
   const [] = useState(0);
 
   return (
-    <div>
+    <div className="d-flex flex-column vh-100">
       <header>
-        Ts mirona
+        <Navbar bg='dark' variant='dark' expand="lg">
+          <Container>
+            <Navbar.Brand>mirona</Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href='/cart' className='nav-link '>Cart</a>
+            <a href='/signin' className='nav-link'>Sign In</a>
+          </Nav>
+        </Navbar>
       </header>
       <main>
-        <ul>
+        <Container className='mt-3'>
+        <Row>
           {SampleProducts.map((product) => (
-            <li key={product.slug}>
-              
-              <img src={product.image} alt={product.name} className='product-image'/>
+            <Col key={product.slug} sm={6} md={4} lg={3}>
+              <img src={product.image} alt={product.name} className='product-image' />
               <h2>{product.name}</h2>
               <p>${product.price}</p>
-              
-            </li>
+            </Col>
           ))}
-        </ul>
+        </Row>
+        </Container>
+       
       </main>
       <footer>
+        <div className="text-center">
         All Rights Reserved
+        </div>
+        
       </footer>
     </div>
   );
