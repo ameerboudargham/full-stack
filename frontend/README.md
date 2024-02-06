96bd6df5452eccc3c1ec364542d5b2a08316109c
ameerboudargham <ameerboudargham@outlook.com>
2024-02-06 20:57:08 +0200

    first commit

---
+++ frontend/README.md
@@ -0,0 +1,6 @@
+# Typescript MERN MIRONA
+## Lessons
+1. Introduction
+2. Install Tools
+3. Create TypeScript React App by vite 
+4. Create Git Repository
---
+++ frontend/src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <div >
+    <header>
+      Ts mirona
+    </header>
+    <main>
+
+    </main>
+    <footer>
+      All Rights Reserved
+    </footer>
+  </div>
+    
+  )
+}
+
+export default App
